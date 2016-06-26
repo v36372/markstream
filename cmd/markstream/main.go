d66bcb2ce64348fab8fa3b8c42447650e5ed3155
Nguyen Trong Tin <nguyentrongtin2331@gmail.com>
2016-06-26 23:40:03 +0700

    + basic ground for new-frontend

--- cmd/markstream/main.go
+++ cmd/markstream/main.go
@@ -5,7 +5,12 @@ import (
   // "net/http"
   // "golang.org/x/net/websocket"
   "github.com/kataras/iris"
+  "github.com/iris-contrib/middleware/logger"
 )
+
+type page struct {
+	Title string
+}
 
 func main() {
 	ms := markstream.NewMarkStream()
@@ -13,10 +18,17 @@ func main() {
 	go ms.Process()
 	go ms.Input()
 
-	http.Handle("/stream", websocket.Handler(ms.StreamServer))
+  iris.Config.Render.Template.Directory = "templates/web/default"
+  iris.Config.Render.Template.Gzip = true
+  iris.OnError(iris.StatusForbidden, func(ctx *iris.Context) {
+		ctx.HTML(iris.StatusForbidden, "<h1> You are not allowed here </h1>")
+	})
+  iris.Static("/css", "./resources/css", 1)
+	iris.Static("/js", "./resources/js", 1)
+  iris.Use(logger.New(iris.Logger))
+  iris.Get("/", func(ctx *iris.Context) {
+		ctx.MustRender("index.html", page{"Hello world"})
+	})
 
-	err := http.ListenAndServe(":8081", nil)
-	if err != nil {
-		panic("ListenAndServe: " + err.Error())
-	}
+	iris.Listen(":8080")
 }
